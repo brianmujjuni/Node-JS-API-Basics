c7dc59a77ebd8a5ed5acc0352ac6a6d66599b53b
Brian <mbrian@projectcode.ug>
2024-09-04 16:48:25 +0300

    configure mongoose connection string

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const path = require('path');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const feedRoutes = require('./routes/feed');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+
+const feedRoutes = require("./routes/feed");
 
 const app = express();
 
@@ -9,12 +13,21 @@ const app = express();
 app.use(bodyParser.json()); // application/json
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
-app.use('/feed', feedRoutes);
+app.use("/feed", feedRoutes);
+const dbUrl = process.env.DB
+mongoose
+  .connect(dbUrl)
+  .then((restult) => {
+    app.listen(8080);
+  }).catch(err => console.log(err))
+  
 
-app.listen(8080);
