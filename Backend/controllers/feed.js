aebef9ae3bfeefcd35d0f1806236ce81caeb3da4
Brian <mbrian@projectcode.ug>
2024-09-04 16:14:34 +0300

    return error after validation in controller

--- Backend/controllers/feed.js
+++ Backend/controllers/feed.js
@@ -1,8 +1,10 @@
+const { validationResult } = require("express-validator");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
       {
-        _id: '1123434566',
+        _id: "1123434566",
         title: "First Post",
         content: "This is the first post!",
         imageUrl: "images/duck.jpg",
@@ -16,11 +18,26 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({
+        messages: "Validation failed,entered data is incorrect",
+        errors: errors.array(),
+      });
+  }
   const title = req.body.title;
   const content = req.body.content;
   // Create post in db
   res.status(201).json({
     message: "Post created successfully!",
-    post: { id: new Date().toISOString(), title: title, content: content ,creator:{name: 'Mujjuni Brian'},createdAt: new Date()},
+    post: {
+      id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: { name: "Mujjuni Brian" },
+      createdAt: new Date(),
+    },
   });
 };
