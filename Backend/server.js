bd489fa4c0ea0a1ac4f4498e0c2e9878ef4c0069
Brian <mbrian@projectcode.ug>
2024-09-05 10:46:48 +0300

    initialise file upload & restrict type of file to upload

--- Backend/server.js
+++ Backend/server.js
@@ -2,12 +2,29 @@ const express = require("express");
 const path = require('path');
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const  multer = require('multer')
 
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const feedRoutes = require("./routes/feed");
 
 const app = express();
+
+const fileStorage = multer.diskStorage({
+  destination: (req,file,cb)=>{
+    cb(null,'images')
+  },
+  filename:(req,file,cb)=>{
+    cb(null,new Date().toISOString() + '-' + file.originalname)
+  }
+})
+const fileFilter = (req,file,cb)=>{
+  if(file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg'){
+    cb(null,true)
+  }else{
+    cb(null,false)
+  }
+}
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
