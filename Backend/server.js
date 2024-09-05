b6e5a1a34fecadf1ea499e5613a470f68a33caec
Brian <mbrian@projectcode.ug>
2024-09-05 10:55:56 +0300

    initalise multer

--- Backend/server.js
+++ Backend/server.js
@@ -28,6 +28,7 @@ const fileFilter = (req,file,cb)=>{
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+app.use(multer({storage: fileStorage,fileFilter: fileFilter}).single('image'))
 app.use('/images',express.static(path.join(__dirname,'images')))
 
 app.use((req, res, next) => {
