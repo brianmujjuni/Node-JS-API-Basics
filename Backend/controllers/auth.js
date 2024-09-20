44c36d522afb9592e8e3bb51e62dbd2eb340dda2
Brian <mbrian@projectcode.ug>
2024-09-20 16:22:45 +0300

    add signup logic

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -1,13 +1,33 @@
-const User = require('../models/user')
-const {validationResult } = require('express-validator')
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const { validationResult } = require("express-validator");
 
-exports.signup = (req,res,next)=>{
-   const errors = validationResult(req)
-   if(!errors.isEmpty()){
-      const error = new Error('validation Failed')
-      error.statusCode = 422
-      error.data = errors.array()
-      throw error;
-   }
-   const {email,name,password} = req.body
-}
+exports.signup = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("validation Failed");
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+  const { email, name, password } = req.body;
+  bcrypt
+    .hash(password, 12)
+    .then((hassedPw) => {
+      const user = new User({
+        email: email,
+        password: hassedPw,
+        name: name,
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res.status(201).json({ message: "User created", userId: result._id });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
