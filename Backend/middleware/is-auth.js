063115ca83c599f9791ccf3661eba3061c10ccd7
Brian <mbrian@projectcode.ug>
2024-09-25 11:09:30 +0300

    complete is auth middleware logic

--- Backend/middleware/is-auth.js
+++ Backend/middleware/is-auth.js
@@ -1,4 +1,18 @@
 const jwt = require('jsonwebtoken')
 module.exports = (req,res,next)=>{
-    
+  const token = req.get('Authorization').split(' ')[1]
+  let decodedToken;
+  try{
+    decodedToken = jwt.verify(token,'ndhfbnejbne1123')
+  }catch(err){
+    err.statusCode = 500
+    throw err
+  }
+  if(!decodedToken){
+    const error = new Error('Not Authenticated')
+    error.statusCode = 401
+    throw error
+  }
+  req.userId = decodedToken.userId
+  next()
 }
