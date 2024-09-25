19bb46794867c5644314f4985a383ba4477f91e9
Brian <mbrian@projectcode.ug>
2024-09-25 11:51:18 +0300

    resolve authorization middleware bug

--- Backend/middleware/is-auth.js
+++ Backend/middleware/is-auth.js
@@ -1,18 +1,26 @@
-const jwt = require('jsonwebtoken')
-module.exports = (req,res,next)=>{
-  const token = req.get('Authorization').split(' ')[1]
+const jwt = require("jsonwebtoken");
+module.exports = (req, res, next) => {
+  const authHeader = req.get("Authorization");
+  if (!authHeader) {
+    const error = new Error("Not authenticated");
+    error.statusCode = 401;
+    throw error;
+  }
+//   const token1="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImJyaWFubXVqanVuaUBnbWFpbC5jb20iLCJ1c2VySWQiOiI2NmVlOGQwZGNhZDg3OTViMjQ1YTIzNjIiLCJpYXQiOjE3MjcyNTMzNzAsImV4cCI6MTcyNzI1Njk3MH0.2E9RR2uGOariZpfmxY9fpWcxWYva6Vqwv7emsfRUPIY"
+  const token = authHeader.split(' ')[1];
+  
   let decodedToken;
-  try{
-    decodedToken = jwt.verify(token,'ndhfbnejbne1123')
-  }catch(err){
-    err.statusCode = 500
-    throw err
+  try {
+    decodedToken = jwt.verify(token, "ndhfbnejbne1123");
+  } catch (err) {
+    err.statusCode = 500;
+    throw err;
   }
-  if(!decodedToken){
-    const error = new Error('Not Authenticated')
-    error.statusCode = 401
-    throw error
+  if (!decodedToken) {
+    const error = new Error("Not Authenticated");
+    error.statusCode = 401;
+    throw error;
   }
-  req.userId = decodedToken.userId
-  next()
-}
+  req.userId = decodedToken.userId;
+  next();
+};
--- Backend/server.js
+++ Backend/server.js
@@ -51,7 +51,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
   next();
 });
 
