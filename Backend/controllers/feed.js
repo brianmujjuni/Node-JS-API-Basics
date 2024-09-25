1ea8b4ff135ce86b7c1e4fc2c77817cdf35387fc
Brian <mbrian@projectcode.ug>
2024-09-25 15:44:25 +0300

    save each post with user logedin

--- Backend/controllers/feed.js
+++ Backend/controllers/feed.js
@@ -1,7 +1,7 @@
 const { validationResult } = require("express-validator");
 
 const Post = require("../models/post");
-
+const User = require("../models/user");
 exports.getPosts = (req, res, next) => {
   const currentPage = req.query.page || 1;
   const perPage = 2;
@@ -41,6 +41,7 @@ exports.createPost = (req, res, next) => {
     error.statusCode = 422;
     throw error;
   }
+  let creator;
   const imageUrl = req.file.path;
   const title = req.body.title;
   const content = req.body.content;
@@ -48,16 +49,26 @@ exports.createPost = (req, res, next) => {
     title: title,
     content: content,
     imageUrl: imageUrl,
-    creator: { name: "Maximilian" },
+    creator: req.userId,
   });
   post
     .save()
     .then((result) => {
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      creator = user;
+      user.posts.push(post);
+      return user.save();
+    })
+    .then((result) => {
       res.status(201).json({
-        message: "Post created successfully!",
-        post: result,
+        message: "Post Created successfully",
+        post: post,
+        creator: { _id: creator._id, name: creator.name },
       });
     })
+
     .catch((err) => {
       if (!err.statusCode) {
         err.statusCode = 500;
